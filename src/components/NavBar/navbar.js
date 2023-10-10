186686db9bf311f5cabf5791ad3f7d113e3b06bf
IzatheGreat15 <20100869@usc.edu.ph>
2023-10-10 18:13:29 +0800

    made it responsive

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/LOGO.png';
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
         <nav className="navbar">
             <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500}><img src={logo} alt="logo" className='logo'/></Link>
@@ -13,6 +14,19 @@ const Navbar = () => {
                 <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Experiences</Link>
                 <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Projects</Link>
                 <Link activeClass='active' to='footer' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Contact</Link>
+            </div>
+
+            <Link activeClass='active' spy={true} smooth={true} offset={-100} duration={500} onClick={()=>setShowMenu(!showMenu)} style={{display: "flex", width: "100%", justifyContent: "end"}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-list mobMenu" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </Link>
+            <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
+                <Link activeClass='active' to='about' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>About</Link>
+                <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Experiences</Link>
+                <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Projects</Link>
+                <Link activeClass='active' to='footer' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Contact</Link>
             </div>
         </nav>
     )   
